f3984e98e93ae712a4123d0986d0a30565cc449e
shivam jaiswar <95872412+S994754@users.noreply.github.com>
2023-09-15 11:06:31 +0000

    commit changes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail ="shivamjasi@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState
+//accountId = 2
+accountEmail = "hc@hc.com"
+accountPassword = "212121"
+accountCity = "Bengaluru"
+
+
+console.log(accountId);
+console.table([accountEmail,accountId, accountPassword, accountCity,accountState])
